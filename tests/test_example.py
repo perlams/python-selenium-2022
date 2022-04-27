bef3235fb3bba93453ac0dbb5681b2df3120865c
perlams <perlams@gmail.com>
2022-04-26 20:29:59 -0500

    update the test_example.py

---
+++ tests/test_example.py
@@ -0,0 +1,13 @@
+class Testsuite:
+
+    def test_first(self):
+        pass
+
+    def test_second(self):
+        pass
+
+    def test_third(self):
+        pass
+
+    def do_something(self):
+        pass
