7e6484eff9f15e2bc9266fcbf5ac58503e462ba8
perlams <perlams@gmail.com>
2022-04-26 19:56:22 -0500

    Initial commit for 01_helloworld.py

---
+++ tests/01_helloworld.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_path = '../drivers/chromedriver'
+gecko_driver_path = '../drivers/geckodriver'
+
+url = 'https://qamindslab.com'
+#service = Service(chrome_driver_path)
+service = Service(gecko_driver_path )
+
+#driver = webdriver.Chrome(service=service)
+driver = webdriver.Firefox(service=service)
+driver.get(url)
+time.sleep(3)
+driver.quit()
